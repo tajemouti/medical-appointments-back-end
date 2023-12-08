8f45663d560d08c3f8e03e363df2f1b4550ab322
tajemouti <tajemouti@gmail.com>
2023-12-08 22:29:11 +0100

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    has_many :appointments
+  has_many :appointments, dependent: :destroy
+  has_many :doctors, through: :appointments
+
+  validates :username, presence: true
 end
