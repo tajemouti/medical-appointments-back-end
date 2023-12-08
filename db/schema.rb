f38687bad34bc7c85a3ead43b1efb4c70adad6ee
Bettenoch <bettenoch254@gmail.com>
2023-12-08 16:01:15 +0300

    Run doctor migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_08_122150) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_08_125319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.string "speciality"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
