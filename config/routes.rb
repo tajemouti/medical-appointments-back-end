cdc8aabffc8fde412a4716470f1f1526eb358b6c
Bettenoch <bettenoch254@gmail.com>
2023-12-11 00:13:13 +0100

    Set up routes for appointments endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :doctors, only: [:index, :show, :create, :destroy]
+      resources :appointments, only: [:create]
       post '/login', to: 'auth#login'
     end
   end
