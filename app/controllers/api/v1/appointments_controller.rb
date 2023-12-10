b499eb56e961d2641cc83666e28ce8dd2ebdaf0e
Bettenoch <bettenoch254@gmail.com>
2023-12-11 00:47:52 +0100

    Fix linters errors

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::AppointmentsController < ApplicationController
-    before_action :authorize_request
-    
-    def index
-      @appointments = @current_user.appointments
-      render json: @appointments
-    end
+  before_action :authorize_request
 
-    def create
-      @appointment = Appointment.new(appointment_params.merge(user: @current_user))
-      
-      if @appointment.save
-        render json: @appointment, status: :created
-      else
-        render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
+  def index
+    @appointments = @current_user.appointments
+    render json: @appointments
+  end
 
-    def show
-      @appointment = @current_user.appointments.find(params[:id])
-      render json: @appointment
-    end
+  def create
+    @appointment = Appointment.new(appointment_params.merge(user: @current_user))
 
-    def destroy
-      @appointment = @current_user.appointments.find(params[:id])
-      @appointment.destroy
-      render json: { message: 'Appointment deleted successfully.' }
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    private
+  def show
+    @appointment = @current_user.appointments.find(params[:id])
+    render json: @appointment
+  end
 
-    def appointment_params
-      params.require(:appointment).permit(:appointment_time, :doctor_id)
-    end
+  def destroy
+    @appointment = @current_user.appointments.find(params[:id])
+    @appointment.destroy
+    render json: { message: 'Appointment deleted successfully.' }
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appointment_time, :doctor_id)
+  end
 end
