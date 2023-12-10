641ca044b5fbcf458d0a1841b10563f17d531be0
wacoo <wabaham9@gmail.com>
2023-12-10 18:22:53 +0100

    Fix linters errors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,38 +1,38 @@
 class Api::V1::DoctorsController < ApplicationController
-    before_action :authorize_request
-    before_action :set_doctor, only: [:show, :destroy]
+  before_action :authorize_request
+  before_action :set_doctor, only: %i[show destroy]
 
-    def index
-      @doctors = Doctor.all
-      render json: @doctors
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
+
+  def show
+    render json: @doctor
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-      render json: @doctor
-    end
+  def destroy
+    @doctor.destroy
+    render json: { message: 'Doctor deleted successfully.' }
+  end
 
-    def create
-      @doctor = Doctor.new(doctor_params)
+  private
 
-      if @doctor.save
-        render json: @doctor, status: :created
-      else
-        render json: @doctor.errors, status: :unprocessable_entity
-      end
-    end
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
 
-    def destroy
-      @doctor.destroy
-      render json: { message: 'Doctor deleted successfully.' }
-    end
-
-    private
-
-    def set_doctor
-      @doctor = Doctor.find(params[:id])
-    end
-
-    def doctor_params
-      params.require(:doctor).permit(:name, :picture, :speciality, :address)
-    end
+  def doctor_params
+    params.require(:doctor).permit(:name, :picture, :speciality, :address)
+  end
 end
