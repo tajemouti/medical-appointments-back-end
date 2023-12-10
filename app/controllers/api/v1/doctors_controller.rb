8c3431002d5834d5909e8c25bd0d51b0f7bc9bbc
wacoo <wabaham9@gmail.com>
2023-12-10 17:58:56 +0100

    Define create action for doctors controller

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::DoctorsController < ApplicationController
+    before_action :authorize_request
+
+    def create
+      @doctor = Doctor.new(doctor_params)
+
+      if @doctor.save
+        render json: @doctor, status: :created
+      else
+        render json: @doctor.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @doctor.destroy
+      render json: { message: 'Doctor deleted successfully.' }
+    end
+
+    private
+
+    def doctor_params
+      params.require(:doctor).permit(:name, :picture, :speciality, :address)
+    end
 end
