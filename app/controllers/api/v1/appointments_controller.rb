5ba89141ab688ad074879c866060ca4549da91e0
Bettenoch <bettenoch254@gmail.com>
2023-12-11 00:11:14 +0100

    Add create action in appointments controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::AppointmentsController < ApplicationController
+    before_action :authorize_request
+
+    def create
+      @appointment = Appointment.new(appointment_params.merge(user: @current_user))
+      
+      if @appointment.save
+        render json: @appointment, status: :created
+      else
+        render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def appointment_params
+      params.require(:appointment).permit(:appointment_time, :doctor_id)
+    end
 end
