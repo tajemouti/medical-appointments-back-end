d62d0c46a01c392e3b316dc55481e30334bcfb4a
tajemouti <tajemouti@gmail.com>
2023-12-11 16:40:29 +0100

    Fix rubocop errors

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-    describe 'validations' do
-      it 'should be valid with valid credentials' do
-        appointment = FactoryBot.create(:appointment)
-        expect(appointment).to be_valid
-      end
+  describe 'validations' do
+    it 'should be valid with valid credentials' do
+      appointment = FactoryBot.create(:appointment)
+      expect(appointment).to be_valid
     end
+  end
 
-    describe 'associations' do
-      it 'should belong to a user' do
-        association = described_class.reflect_on_association(:user)
-        expect(association.macro).to eq :belongs_to
-      end
+  describe 'associations' do
+    it 'should belong to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq :belongs_to
     end
+  end
 end
