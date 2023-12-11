3cf8ce2bf491d160d1f369a611ec9e574356f6b9
wac <wabaham9@gmail.com>
2023-12-11 17:49:25 +0300

    Add more validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe User, type: :model do
       user = FactoryBot.create(:user)
       expect(user).to be_valid
     end
+
+    it 'is not valid without a username' do
+      user = User.new(username: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      user = User.new(username: 'example_user', password: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid with a password less than 6 characters' do
+      user = User.new(username: 'example_user', password: '12345')
+      expect(user).to_not be_valid
+    end
+
   end
 
   # test user associations
