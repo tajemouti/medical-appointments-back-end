b33d47e3a5fca6010812a9c020f5c749d621e5fc
wac <wabaham9@gmail.com>
2023-12-12 12:05:13 +0300

    Update json data for create user test

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -5,7 +5,20 @@ RSpec.describe 'api/v1/users', type: :request do
   path '/api/v1/users' do
 
     post('create user') do
-      response(200, 'successful') do
+      consumes 'application/json'
+      parameter name: :user_params, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            username: { type: :string },
+            password: { type: :string }
+          }
+        },
+        required: ['username', 'password']
+      }
+
+      response '200', 'successful' do
+        let(:user_params) { FactoryBot.attributes_for(:user) }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -14,6 +27,11 @@ RSpec.describe 'api/v1/users', type: :request do
             }
           }
         end
+        run_test!
+      end
+
+      response '422', 'unprocessable entity' do
+        let(:user_params) { { username: nil, password: nil } }
         run_test!
       end
     end
