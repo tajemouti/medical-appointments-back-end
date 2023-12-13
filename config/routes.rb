83965c2d4cf685b19563232e67269f80faf428a8
Bettenoch <bettenoch254@gmail.com>
2023-12-13 11:45:24 +0300

    Add api docs  root route.

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  # get "up" => "rails/health#show", as: :rails_health_check
+  root to: redirect('/api-docs')
   # Defines the root path route ("/")
   # root "posts#index"
 end
