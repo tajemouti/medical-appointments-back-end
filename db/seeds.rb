a01bf183a1d1a9aa750fe126e8da81993d504d0e
Bettenoch <bettenoch254@gmail.com>
2023-12-16 23:20:53 +0100

    Udpade database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,11 @@ User.create(username: "Shah", password: "123456")
 User.create(username: "Enoch", password: "123456")
 User.create(username: "Wonde", password: "123456")
 
-Doctor.create(name: 'Dr. Patrick Soon Shiong', picture: 'https://drive.google.com/file/d/1R6Aoue9_MRJKk8noMdjVaWP3HUy_sEMe/view?usp=sharing', speciality: 'Transplant surgeon', address: '123 Main Street City')
-
-Doctor.create(name: 'Dr. William A. Abdu', picture: 'https://drive.google.com/file/d/1ePeL6c2umJ7cN6SWAEz1cBOW_iNzyFD4/view?usp=sharing', speciality: 'Spine and orthopedic surgeon', address: '456 Main Street City')
-
-Doctor.create(name: 'Dr. Myles. B. Abbott', picture: 'https://drive.google.com/file/d/1ALfHTda0fTXSzLUtk-RAUnpp__XlXhQ9/view?usp=sharing', speciality: 'Pediatrician', address: '789 Main Street City')
-
-Doctor.create(name: 'Dr. Fouad. M. Abbas', picture: 'https://drive.google.com/file/d/1qUKD-vMTqKCUxjdBY1irWnFLRQHdK2f-/view?usp=sharing', speciality: 'Gynecologist and Obstetrician', address: '321 Main Street City')
-
-Doctor.create(name: 'Dr. Khalid Abbed', picture: 'https://drive.google.com/file/d/1Bw2Blp4jc9Paq_w8DlNz8GhAQGAmity8/view?usp=sharing', speciality: 'Neuro and spine surgeon', address: '654 Main Street City')
+Doctor.create(name: 'Dr. Patrick Soon Shiong', picture: 'https://img.freepik.com/free-photo/doctor-with-his-arms-crossed-white-background_1368-5789.jpg', speciality: 'Transplant surgeon', address: '123 Main Street City')
+Doctor.create(name: 'Dr. William A. Abdu', picture: 'https://img.freepik.com/premium-photo/happy-male-doctor-standing-with-arms-folded-isolated-white-background_171337-77006.jpg', speciality: 'Spine and orthopedic surgeon', address: '456 Main Street City')
+Doctor.create(name: 'Dr. Myles. B. Abbott', picture: 'https://img.freepik.com/free-photo/portrait-confident-male-doctor_329181-12190.jpg', speciality: 'Pediatrician', address: '789 Main Street City')
+Doctor.create(name: 'Dr. Fouad. M. Abbas', picture: 'https://img.freepik.com/free-photo/young-beautiful-successful-female-doctor-with-stethoscope-portrait_186202-1506.jpg', speciality: 'Gynecologist and Obstetrician', address: '321 Main Street City')
+Doctor.create(name: 'Dr. Khalid Abbed', picture: 'https://img.freepik.com/free-photo/young-medical-student_144627-9948.jpg', speciality: 'Neuro and spine surgeon', address: '654 Main Street City')
 
 
 Appointment.create(appointment_time: DateTime.now + 10.day, user_id: 1, doctor_id: 1)
