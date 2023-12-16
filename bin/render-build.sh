f1f5526a6ad80dfa93583aa988bc4e43da65016e
Bettenoch <bettenoch254@gmail.com>
2023-12-16 22:41:55 +0100

    Restore render script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:drop
-bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
